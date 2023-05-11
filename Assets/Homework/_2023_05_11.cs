1f55df044710357d64b362f46f6dca4cfcc4f178
Dongyeon Shin <d.shin0920@gmail.com>
2023-05-11 21:45:13 +0900

    feat: 2023-05-11 Lesson and Assignment
    유니티 엔진, 오브젝트, 콤포넌트, 메시지이벤트

---
+++ Assets/Homework/_2023_05_11.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class _2023_05_11 : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    [SerializeField]
+    [Range(0, 10)]
+    private float jumpPower = 5.0f;
+    private void Awake()
+    {
+        if (rigidBody == null)
+        {
+            rigidBody = GetComponent<Rigidbody>();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 1)
+        {
+            SceneManager.LoadScene(1);
+        }
+        gameObject.name = "Player";
+        rigidBody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
